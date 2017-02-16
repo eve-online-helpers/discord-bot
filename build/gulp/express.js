2f717c8646c577c6006ca2c9bb3b310af2c5237f
Ashtar Veres <ashtar.veres@gmail.com>
2017-02-16 18:41:39 +0200

    fixed env on build

--- build/gulp/express.js
+++ build/gulp/express.js
@@ -1,6 +1,13 @@
 const server = require('gulp-express');
 const gulp = require('gulp');
-const env = require('../../env.json');
+
+const env
+try {
+    env = require('../../env.json');
+}
+catch (e) {
+    console.warn('env not found will search env variables');
+}
 
 gulp.task('express', () => {
     server.run(['--debug', './bin/www'], {
