01d339db3300b40a91787266f840e9ccf018902f
Ashtar Veres <ashtar.veres@gmail.com>
2017-02-18 21:10:49 +0200

    added coverage to test task

--- build/gulp/mocha.js
+++ build/gulp/mocha.js
@@ -1,14 +1,36 @@
 const gulp = require('gulp');
 const mocha = require('gulp-mocha');
+const cover = require('gulp-coverage');
+const gulp_env = require('gulp-env');
+const coveralls = require('gulp-coveralls');
+
+let env;
+try {
+    env = require('../../env.json');
+}
+catch (e) {
+    console.warn('env not found will search env variables');
+}
 
 gulp.task('mocha', () => {
     return gulp.src(['**/*.spec.js', '!node_modules/**'], { read: false })
         // gulp-mocha needs filepaths so you can't have any plugins before it
-        .pipe(mocha({ reporter: 'nyan' }))
+        .pipe(mocha({ reporter: 'nyan' }));
 });
 
 gulp.task('mocha-travis', () => {
     return gulp.src(['**/*.spec.js', '!node_modules/**'], { read: false })
+        .pipe(cover.instrument({
+            pattern: ['**/*.js', '!node_modules/**', '!**/*.spec.js'],
+
+        }))
         // gulp-mocha needs filepaths so you can't have any plugins before it
         .pipe(mocha({ reporter: 'min' }))
+        .pipe(cover.gather())
+        .pipe(gulp_env({
+            vars: env
+        }))
+        .pipe(cover.format({ reporter: 'lcov' }))
+        // .pipe(gulp.dest('reports'))
+        .pipe(coveralls());
 });
