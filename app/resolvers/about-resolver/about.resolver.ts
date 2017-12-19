a8fe7b8e04f53f22ad9f403191c52e4119b64a3a
Dima <dimkinv@gmail.com>
2017-12-19 22:44:23 +0200

    added docker container build

--- app/resolvers/about-resolver/about.resolver.ts
+++ app/resolvers/about-resolver/about.resolver.ts
@@ -1,8 +1,8 @@
 import { ParsedInput } from '../../models/parsed-input.model';
 
-const version = require('../../../version.json').version;
+// const version = require('../../../version.json').version;
 export function aboutResolver(input: ParsedInput): Promise<string> {
-    let result = `\`\`\`EVE discord bot, version ${version}\`\`\``;
+    let result = `\`\`\`EVE discord bot, version alpha\`\`\``;
 
     return new Promise<string>((resolve, reject) => {
         resolve(result);
